84743b85f93540bfd9d2e6cfce6646032a3d4dee
lutfiqaraman <lutfiqaraman@gmail.com>
2021-06-07 23:11:45 +0300

    do a diagram with basic feature

--- project1/js/buildings.js
+++ project1/js/buildings.js
@@ -5,45 +5,6 @@ const svg =
         .attr('width', 900)
         .attr('height', 400);
 
-const randomColor = (function () {
-    const golden_ratio_conjugate = 0.618;
-    let h = Math.random();
-
-    const hslToRgb = function (h, s, l) {
-        let r, g, b;
-
-        if (s === 0) {
-            r = g = b = l; // achromatic
-        } else {
-            function hue2rgb(p, q, t) {
-                if (t < 0) t += 1;
-                if (t > 1) t -= 1;
-                if (t < 1 / 6) return p + (q - p) * 6 * t;
-                if (t < 1 / 2) return q;
-                if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-                return p;
-            }
-
-            const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-            const p = 2 * l - q;
-            r = hue2rgb(p, q, h + 1 / 3);
-            g = hue2rgb(p, q, h);
-            b = hue2rgb(p, q, h - 1 / 3);
-        }
-
-        return '#'
-            + Math.round(r * 255).toString(16)
-            + Math.round(g * 255).toString(16)
-            + Math.round(b * 255).toString(16);
-    };
-
-    return function () {
-        h += golden_ratio_conjugate;
-        h %= 1;
-        return hslToRgb(h, 0.5, 0.60);
-    };
-})();
-
 d3.json('data/buildings.json')
     .then(chartData => {
 
@@ -55,12 +16,14 @@ d3.json('data/buildings.json')
             .selectAll('rect')
             .data(chartData);
 
-        rects.enter().append('rect')
-            .attr('y', (d, i) => (i * 60) + 10)
-            .attr('x', 0)
-            .attr('width', (d) => d.height)
-            .attr('height', 50)
-            .attr('fill', () => randomColor())
+        rects
+            .enter()
+            .append('rect')
+            .attr('x', (d, i) => (i * 60) + 10)
+            .attr('y', 0)
+            .attr('width', 50)
+            .attr('height', (d) => d.height)
+            .attr('fill', () => 'grey')
     })
     .catch((err) => {
         console.log(err);
